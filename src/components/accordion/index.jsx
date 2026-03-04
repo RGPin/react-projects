20bbb45e5d2d399ab67c192fc2189b272edb7ea4
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-04 11:50:35 +0800

    Accordion: handle single selection

--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -3,16 +3,28 @@ import data from "./data";
 
 const Accordian = () => {
   const [selected, setSelected] = useState(null);
+
+  const handleSingleSelection = (currentId) => {
+    setSelected(currentId === selected ? null : currentId);
+  };
+
   return (
     <div className="wrapper">
       <div className="accordian">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
-            <div className="item">
+            <div
+              onClick={() => handleSingleSelection(dataItem.id)}
+              className="item"
+              key={dataItem.id}
+            >
               <div className="title">
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
+              {selected === dataItem.id ? (
+                <div className="content">{dataItem.answer}</div>
+              ) : null}
             </div>
           ))
         ) : (
