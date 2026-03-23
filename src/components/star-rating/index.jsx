86e5eb42a0bee31e6a552ba1922251bc1647d90a
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-23 14:45:04 +0800

    StarRating: change bg color of stars on hover and click

--- src/components/star-rating/index.jsx
+++ src/components/star-rating/index.jsx
@@ -1,20 +1,21 @@
 import { FaStar } from "react-icons/fa";
 import { useState } from "react";
+import "./styles.css";
 
 export default function StarRating({ noOfStars = 5 }) {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   function handleClick(index) {
-    console.log(index);
+    setRating(index);
   }
 
   function handleMouseEnter(index) {
-    console.log(index);
+    setHover(index);
   }
 
-  function handleMouseLeave(index) {
-    console.log(index);
+  function handleMouseLeave() {
+    setHover(rating);
   }
 
   return (
@@ -25,9 +26,10 @@ export default function StarRating({ noOfStars = 5 }) {
           return (
             <FaStar
               key={index}
+              className={index <= (hover || rating) ? "active" : "inactive"}
               onClick={() => handleClick(index)}
               onMouseMove={() => handleMouseEnter(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
+              onMouseLeave={() => handleMouseLeave()}
               size={40}
             />
           );
