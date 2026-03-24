d1dfe6fffec15e197f1191be0b4792ddc467ce00
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-24 12:49:07 +0800

    LoadMoreData: forgot to commit. Here summary: fetch data from dummyjson, add styling, encounter duplicated products due to strict mode, create AbortController object to... abort the first fetch of useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Accordian from "./components/accordion";
 import ImageSlider from "./components/image-slider";
+import LoadMoreData from "./components/load-more-data";
 import RandomColor from "./components/random-color";
 import StarRating from "./components/star-rating";
 
@@ -9,11 +10,12 @@ function App() {
       {/* <Accordian /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         limit={"10"}
         page={"1"}
-      />
+      /> */}
+      <LoadMoreData />
     </>
   );
 }
---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+import "./styles.css";
+
+export default function LoadMoreData() {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+
+  async function fetchProducts(signal) {
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${count === 0 ? 0 : count * 20}`,
+        { signal },
+      );
+      const result = await response.json();
+      if (result && result.products && result.products.length) {
+        setProducts((prevData) => [...prevData, ...result.products]);
+        setLoading(false);
+      }
+    } catch (e) {
+      console.log(e.message);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetchProducts(signal);
+    return () => controller.abort();
+  }, [count]);
+
+  if (loading) {
+    return <div>Loading data! Please wait.</div>;
+  }
+
+  return (
+    <div className="load-more-container">
+      <div className="product-container">
+        {products && products.length
+          ? products.map((item) => (
+              <div key={item.id} className="product">
+                <img src={item.thumbnail} alt={item.title} />
+                <p>{item.title}</p>
+              </div>
+            ))
+          : null}
+      </div>
+      <div className="button-container">
+        <button onClick={() => setCount(count + 1)}>Load More Products</button>
+      </div>
+    </div>
+  );
+}
