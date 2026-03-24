c753429e99d0ead04991a668a0c82598fc727a8b
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-24 14:23:13 +0800

    TreeView: add collapsible functionality of children

--- src/components/tree-view/menu-item.jsx
+++ src/components/tree-view/menu-item.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import MenuList from "./menu-list";
 
 export default function MenuItem({ item }) {
+  const [displayChildren, setDisplayChildren] = useState({});
+  function handleToggleChildren(label) {
+    setDisplayChildren({
+      ...displayChildren,
+      [label]: !displayChildren[label],
+    });
+  }
   return (
     <li>
-      <p>{item.label}</p>
-      {item && item.children && item.children.length > 0 ? (
+      <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
+        <p>{item.label}</p>
+        {item && item.children && item.children.length ? (
+          <span onClick={() => handleToggleChildren(item.label)}>
+            {displayChildren[item.label] ? "-" : "+"}
+          </span>
+        ) : null}
+      </div>
+      {item &&
+      item.children &&
+      item.children.length > 0 &&
+      displayChildren[item.label] ? (
         <MenuList list={item.children} />
       ) : null}
     </li>
