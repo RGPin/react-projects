fb27f9378805103909de9184a24f7edab82a3c14
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-27 12:11:10 +0800

    ProfileFinder: fetch data, add loading and error states

--- src/components/github-profile-finder/index.jsx
+++ src/components/github-profile-finder/index.jsx
@@ -1,9 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function GithubProfileFinder() {
-  const [userName, setUserName] = useState();
+  const [userName, setUserName] = useState("sangammukherjee");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  // const [userData, setUserData] = useState();
+
+  async function fetchGithubUserData() {
+    setLoading(true);
+    try {
+      const response = await fetch(`https://api.github.com/users/${userName}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   function handleSubmit() {}
+
+  useEffect(() => {
+    fetchGithubUserData();
+  }, []);
+
+  if (loading) return <div>Loading! Please wait...</div>;
+
+  if (error) return <div>Error! {error}</div>;
 
   return (
     <div className="github-profile-container">
