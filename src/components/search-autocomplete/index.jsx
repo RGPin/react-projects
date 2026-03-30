19a0de9ec984fea6c7b06ff138a129e60aa9e2e1
RGPin <157803400+RGPin@users.noreply.github.com>
2026-03-30 15:20:06 +0800

    SearchAutocomplete: store fetched data in state, filter based on input, log

--- src/components/search-autocomplete/index.jsx
+++ src/components/search-autocomplete/index.jsx
@@ -4,6 +4,25 @@ export default function SearchAutoComplete() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
   const [users, setUsers] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
+  function handleChange(e) {
+    const query = e.target.value.toLowerCase();
+    setSearchInput(query);
+    if (query.length) {
+      const filteredData =
+        users && users.length
+          ? users.filter((user) => user.toLowerCase().includes(query))
+          : [];
+      setFilteredUsers(filteredData);
+      setShowDropdown(true);
+    } else {
+      setFilteredUsers([]);
+      setShowDropdown(false);
+    }
+  }
 
   async function fetchData() {
     try {
@@ -13,7 +32,8 @@ export default function SearchAutoComplete() {
       if (!response.ok) throw new Error("Failed to fetch");
       const data = await response.json();
 
-      console.log(data);
+      if (data && data.users && data.users.length)
+        setUsers(data.users.map((user) => user.firstName));
     } catch (error) {
       console.error(error);
       setError(error);
@@ -26,6 +46,8 @@ export default function SearchAutoComplete() {
     fetchData();
   }, []);
 
+  console.log(filteredUsers);
+
   if (loading) return <h1>Loading. Please wait...</h1>;
 
   if (error) return <div>Error: {error}</div>;
@@ -33,9 +55,11 @@ export default function SearchAutoComplete() {
   return (
     <div className="search-autocomplete-container">
       <input
+        value={searchInput}
         name="search-users"
         placeholder="Search Users here..."
         type="text"
+        onChange={handleChange}
       />
     </div>
   );
