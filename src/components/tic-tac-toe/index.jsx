786cd1be4913613d7456682e7e9e94295e02435a
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-03 15:41:49 +0800

    TicTacToe: add option to restart

--- src/components/tic-tac-toe/index.jsx
+++ src/components/tic-tac-toe/index.jsx
@@ -43,6 +43,11 @@ export default function TicTacToe() {
     setSquares(copySquares);
   }
 
+  function handleRestart() {
+    setIsXTurn(true);
+    setSquares(Array(9).fill(""));
+  }
+
   useEffect(() => {
     if (!checkWinner(squares) && squares.every((item) => item !== "")) {
       setStatus("Draw!");
@@ -71,6 +76,7 @@ export default function TicTacToe() {
         <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
       <h1>{status}</h1>
+      <button onClick={handleRestart}>Restart</button>
     </div>
   );
 }
