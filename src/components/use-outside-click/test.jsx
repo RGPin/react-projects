06fd8639afd746749f6b1474b00e5ed30aad17c7
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-06 13:49:33 +0800

    UseOutsideClickHook: create hook, use hook

--- src/components/use-outside-click/test.jsx
+++ src/components/use-outside-click/test.jsx
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import useOutsideClick from ".";
 
 export default function UseOnClickOutsideTest() {
+  const ref = useRef();
   const [showContent, setShowContent] = useState(false);
+  useOutsideClick(ref, () => setShowContent(false));
 
   return (
     <div>
       {showContent ? (
-        <div>
+        <div ref={ref}>
           <h1>This is a random conent</h1>
           <p>Click outside to close this.</p>
         </div>
       ) : (
-        <button onClick={() => setShowContent(true)}></button>
+        <button onClick={() => setShowContent(true)}>Show content</button>
       )}
     </div>
   );
