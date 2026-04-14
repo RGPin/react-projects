892e6a4073e1c9af3b7a8417687c32d20e022a4b
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-14 23:39:58 +0800

    WeatherApp: states, dummy funcs

--- src/components/weather-app/weather/index.jsx
+++ src/components/weather-app/weather/index.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import Search from "../search";
 
 export default function Weather() {
+  const apiKey = import.meta.env.VITE_WEATHER_KEY;
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [weatherData, setWeatherData] = useState(null);
+
+  async function fetchWeatherData() {}
+
+  async function handleSearch() {}
+
   return (
     <div>
-      <Search />
+      <Search
+        search={search}
+        setSearch={setSearch}
+        handleSearch={handleSearch}
+      />
       Weather
     </div>
   );
