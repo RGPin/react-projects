ea520476d790ccfe9d12de07276b059ebaaeeda1
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-15 17:15:13 +0800

    ShoppingCart: fetch products, display products, loading and error state

--- src/components/shopping-cart/pages/Home.jsx
+++ src/components/shopping-cart/pages/Home.jsx
@@ -1,3 +1,57 @@
+import { useEffect, useState } from "react";
+import { Circles } from "react-loader-spinner";
+import ProductTile from "../components/product-tile";
+
 export default function Home() {
-  return <div>Home</div>;
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function fetchProducts(signal) {
+    try {
+      setLoading(true);
+      const response = await fetch("https://fakestoreapi.com/products", {
+        signal,
+      });
+      if (!response.ok) throw new Error(response.statusText);
+      const data = await response.json();
+      setProducts(data);
+    } catch (e) {
+      if (e.name !== "AbortError") {
+        console.error(e);
+        setError(e);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    fetchProducts(signal);
+
+    return () => controller.abort();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <div className="min-h-screen w-full flex justify-center items-center">
+          <Circles
+            height={"120"}
+            width={"120"}
+            color="rgb(127,29,29)"
+            visible={true}
+          />
+        </div>
+      ) : error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div className="min-h-[80vh] grid sm:grid-cols-2 md:grid-cols-3 space-x-5 space-y-10 lg:grid-cols-4 max-w-6xl mx-auto p-3">
+          {products?.length &&
+            products.map((product) => <ProductTile product={product} />)}
+        </div>
+      )}
+    </div>
+  );
 }
