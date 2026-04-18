fcf4159fc8acb3b09da59e798b37ff0a840d3152
RGPin <157803400+RGPin@users.noreply.github.com>
2026-04-18 14:37:06 +0800

    ExpenseTracker: simple main page

--- src/components/expense-tracker/ExpenseTrackerApp.jsx
+++ src/components/expense-tracker/ExpenseTrackerApp.jsx
@@ -3,7 +3,7 @@ import Main from "./components/main";
 
 export default function ExpenseTrackerApp() {
   return (
-    <Container bg={"#f8fafd"} maxW={"Container.3xl"} height={"100vh"} p={"0"}>
+    <Container bg={"#f8fafd"} maxW={"120rem"} height={"100vh"} p={"0"}>
       <Flex height={"full"}>
         <Box height={"full"} flex={5} w={["20%", "30%", "20%", "50%", "60%"]}>
           <Main />
